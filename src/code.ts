fe52569ce9f26b6366d985329bc3202ea301baa5
Graham Sysko <gsysko@gmail.com>
2021-05-14 13:46:07 +0100

    Add details section

--- src/code.ts
+++ src/code.ts
@@ -5,6 +5,9 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
 
+var listFrame: FrameNode
+var detailsFrame: FrameNode
+
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__)
 figma.ui.resize(400, 400)
@@ -16,93 +19,144 @@ figma.ui.onmessage = async msg => {
 
   switch (msg.type) {
     case "create-projct":
-      await createProject(msg.projectTitle, msg.projectType);
-      break;
+      await createProject(msg.projectTitle, msg.projectType, msg.projectDescription)
+      break
   }
 }
 
-async function createProject(title, type) {
-  // Create pages
-  let problemPage = figma.createPage();
-  let solutionPage = figma.createPage();
-  let specPage = figma.createPage();
-  let researchPage = figma.createPage();
-  let prototypePage = figma.createPage();
-  let archivePage = figma.createPage();
-
+const PADDING_H = 40
+const PADDING_V = 40
+const SPACING = 24
+const FONT_TITLES = { family: "Menlo", style: "Regular" }
+const FONT_BODIES = { family: "SF Pro Text", style: "Regular" }
+async function createProject(title, type, description) {
   // Set page names and renames the default "Page 1"
-  figma.currentPage.name = "📖 About";
-  problemPage.name = "🤔 Problem Space";
-  solutionPage.name = "💡 Solution Space";
-  specPage.name = "📐 Design Spec";
-  researchPage.name = "🔬 Research";
-  prototypePage.name = "🕹 Prototype";
-  archivePage.name = "📦 Archive";
+  figma.currentPage.name = "📖 About"
+  createPage("🤔 Problem Space")
+  createPage("💡 Solution Space")
+  createPage("📐 Design Specs")
+  createPage("         - Backlog")
+  createPage("         - In development")
+  createPage("         - Shipped")
+  createPage("🔬 Research")
+  createPage("🕹 Prototype")
+  createPage("📦 Archive")
+
+  // Need to load a font here to generate the other page examples.
+  await figma.loadFontAsync(FONT_TITLES)
+  await figma.loadFontAsync(FONT_BODIES)
+
+  //Add a thumnail to the first page.
+  await createThumbnail(title, type)
+
+  // Frame for project details.
+  detailsFrame = figma.createFrame()
+  detailsFrame.name = "Project details"
+  detailsFrame.y = 340
+  detailsFrame.resizeWithoutConstraints(640, 1)
+  detailsFrame.layoutMode = "VERTICAL"
+  detailsFrame.counterAxisSizingMode = "FIXED"
+  detailsFrame.verticalPadding = PADDING_V
+  detailsFrame.horizontalPadding = PADDING_H
+  detailsFrame.itemSpacing = SPACING
+  figma.currentPage.appendChild(detailsFrame)
+
+  await createDetail("Description", description !== "" ? description : "<Enter a description here>")
+  await createDetail("External Links", "<Add links here> →\n<Add links here> →\n<Add links here> →")
+  await createDetail("Slack Channels", "#<channel name here>\n#<channel name here>")
+  await createDetail("Points of Contact", "Design - <link Slack profile here>\nProduct - <link Slack profile here>\nEngineering - <link Slack here>")
 
   // Frame for wrapping the list of page examples.
-  let listFrame = figma.createFrame();
-  listFrame.name = "You can add other pages, like these...";
-  listFrame.y = 340
+  listFrame = figma.createFrame()
+  listFrame.name = "Add other pages, as needed..."
+  listFrame.y = detailsFrame.y + detailsFrame.height + SPACING
   listFrame.resizeWithoutConstraints(640, 1)
-  listFrame.layoutMode = "VERTICAL";
-  listFrame.counterAxisSizingMode = "FIXED";
-  listFrame.verticalPadding = 16;
-  listFrame.horizontalPadding = 16;
-  listFrame.itemSpacing = 16;
-  figma.currentPage.appendChild(listFrame);
+  listFrame.layoutMode = "VERTICAL"
+  listFrame.counterAxisSizingMode = "FIXED"
+  listFrame.verticalPadding = PADDING_V
+  listFrame.horizontalPadding = PADDING_H
+  listFrame.itemSpacing = 8
+  figma.currentPage.appendChild(listFrame)
 
-  let run = async () => {
-    // Need to load a font here to generate the other page examples.
-    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
-    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+  // Not all projects need a prototype, shipped it/released, or research page.
+  // However in order to make adding one of these pages easily, we add some
+  // text to our scratch page so we can copy/paste them with the proper emoji.
+  await createPageExample("🚢 Shipped")
+  await createPageExample("💅🏽 Styles")
+  await createPageExample("⚙️ Components")
+  await createPageExample("👀 Ready for Review")
 
-    //Add a thumnail to the first page.
-    await createThumbnail(title, type);
+  figma.closePlugin()
+}
 
-    // Not all projects need a prototype, shipped it/released, or research page.
-    // However in order to make adding one of these pages easily, we add some
-    // text to our scratch page so we can copy/paste them with the proper emoji.
-    await createAdditionalPageExample("🚢 Shipped");
-    await createAdditionalPageExample("💅🏽 Styles");
-    await createAdditionalPageExample("⚙️ Components");
-    await createAdditionalPageExample("👀 Ready for Review");
-  }
+// This function adds a thumbnail to your first page.
+function createThumbnail(title: string, type: string) {
+  figma.importComponentByKeyAsync("ac0b158c37de3fa8ba94d2b3801913aea262ffcb").catch(reason => {
+    figma.notify("Annotation Kit library is required for thumbnails.")
+    figma.closePlugin()
+  }).then(async component => {
+    let thumbnailFrame = figma.createFrame()
+    thumbnailFrame.name = "Thumbnail - Right click to \"Set as thumbnail\""
+    thumbnailFrame.resizeWithoutConstraints(640, 320)
 
-  // This function adds an example of how to name your less common pages + their emoji
-  // to your first page.
-  let createAdditionalPageExample = (text) => {
-    let linkLabel = figma.createText();
-    linkLabel.fontName = { family: "Inter", style: "Regular" };
-    linkLabel.characters = text;
-    linkLabel.fontSize = 16;
-    listFrame.appendChild(linkLabel);
-  };
+    if (component) {
+      let thumbnail = component.createInstance()
+      let label = thumbnail.findOne(node => node.name == "File Name") as TextNode
+      await figma.loadFontAsync(label.fontName as FontName).then(() => {
+        if (title !== ""){
+          label.characters = title
+        } else {
+          label.characters = "Enter title here"
+        }
+      })
+      let badge = thumbnail.findOne(node => node.name == "Badge" && node.type == "TEXT") as TextNode
+      await figma.loadFontAsync(badge.fontName as FontName).then(() => {
+        badge.characters = type
+      })
+      thumbnailFrame.appendChild(thumbnailFrame)
 
-  // This function adds a thumbnail to your first page.
-  let createThumbnail = (title, type) => {
-    figma.importComponentByKeyAsync("ac0b158c37de3fa8ba94d2b3801913aea262ffcb").catch(reason => {
-      figma.notify("Annotation Kit library is required for thumbnails.")
-      figma.closePlugin();
-    }).then(async component => {
-      if (component) {
-        let thumbnail = component.createInstance()
-        await figma.loadFontAsync({family: "Sharp Sans No1", style: "Bold"}).then(() => {
-          let label = thumbnail.findOne(node => node.name == "File Name") as TextNode
-          if (title !== ""){
-            label.characters = title
-          } else {
-            label.characters = "Enter title here"
-          }
-        })
-        await figma.loadFontAsync({family: "Proxima Nova", style: "Regular"}).then(() => {
-          let badge = thumbnail.findOne(node => node.name == "Badge" && node.type == "TEXT") as TextNode
-          badge.characters = type
-        })
-        figma.currentPage.appendChild(thumbnail)  
-        figma.closePlugin();
-      }
-    })
-  };
+      figma.currentPage.appendChild(thumbnailFrame)
+    }
+  })
+}
 
-  run();
+// Adds a new page.
+function createPage(title: string) {
+  let page = figma.createPage()
+  page.name = title
+}
+
+// Adds a section to your details frame.
+function createDetail(title: string, body: string) {
+  let detailFrame = figma.createFrame()
+  detailFrame.name = title
+  detailFrame.layoutMode = "VERTICAL"
+  detailFrame.counterAxisSizingMode = "AUTO"
+  detailFrame.layoutAlign = "STRETCH"
+  detailFrame.itemSpacing = 8
+
+  let titleText = figma.createText()
+  titleText.fontName = FONT_TITLES
+  titleText.fontSize = 17
+  titleText.characters = title
+  titleText.layoutAlign = "STRETCH"
+  detailFrame.appendChild(titleText)
+
+  let bodyText = figma.createText()
+  bodyText.fontName = FONT_BODIES
+  bodyText.fontSize = 14
+  bodyText.characters = body
+  bodyText.layoutAlign = "STRETCH"
+  detailFrame.appendChild(bodyText)
+
+  detailsFrame.appendChild(detailFrame)
+}
+
+// Adds an example to your list frame.
+function createPageExample(text: string) {
+  let linkLabel = figma.createText()
+  linkLabel.fontName = FONT_BODIES
+  linkLabel.fontSize = 14
+  linkLabel.characters = text
+  listFrame.appendChild(linkLabel)
 }
