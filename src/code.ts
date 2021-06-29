7cb3c80e8cc6e2d22f412cc537e0396ede4ccf82
Graham Sysko <gsysko@gmail.com>
2021-06-29 12:05:23 +0100

    Small improvemnts

--- src/code.ts
+++ src/code.ts
@@ -28,6 +28,7 @@ if(figma.root.getPluginData("status") == "run") {
 figma.ui.onmessage = async msg => {
   switch (msg.type) {
     case "create-projct":
+      figma.ui.hide()
       await createProject(msg.projectTitle, msg.projectType, msg.projectDescription)
       figma.root.setRelaunchData({about: "This document was formated with Ztart"})
       figma.root.setPluginData("status", "run")
@@ -86,7 +87,7 @@ async function createProject(title, type, description) {
 
   //Add a thumnail to the first page.
   await createThumbnail(title, type).then(async () => {
-    createProjectDetails(description)
+    createProjectDetails(description, type)
   })
   if (type == "Library"){
     let targets: FrameNode[] = [await createUse(), await createContribute()]
@@ -96,7 +97,7 @@ async function createProject(title, type, description) {
   figma.closePlugin()
 }
 
-function createProjectDetails(description) {
+function createProjectDetails(description, type) {
   detailsFrame = figma.createFrame()
   detailsFrame.name = "Project details"
   detailsFrame.y = 340
@@ -128,9 +129,20 @@ function createProjectDetails(description) {
   // Not all projects need a prototype, shipped it/released, or research page.
   // However in order to make adding one of these pages easily, we add some
   // text to our scratch page so we can copy/paste them with the proper emoji.
-  createPageExample("💅🏽 Styles")
-  createPageExample("⚙️ Components")
-  createPageExample("👀 Ready for Review")
+  switch (type) {
+    case "Exploration":
+      createPageExample("⏳ History")
+      createPageExample("✅ Next steps")
+    break
+    case "Product":
+      createPageExample("💅🏽 Styles")
+      createPageExample("⚙️ Components")
+    break
+    case "Library":
+      createPageExample("💅🏽 Styles")
+      createPageExample("🚀 Roadmap")
+    break
+  }
   figma.viewport.scrollAndZoomIntoView(figma.currentPage.children);
 }
 
@@ -232,9 +244,9 @@ async function createUse() {
   figma.currentPage = usePage
 
   let frame1 = await createSlideFrame(TEMPLATE_CONTENTS, "Using this library", "", "1. Add sections here\n\nOptionally, add more sections to help describe the usage of your library.")
-  await addContent(frame1, "Purpose", await createSlideFrame(TEMPLATE_INFO, "Contributing", "Purpose", "2. Describe the purpose\n\nThis library was created to fill a need. Describe that need and let designers what does (and doesn’t) fit within this library.\n\nYou can also add an image to the right that represents your library.", "E.g. The Conversation Kit is a Figma library - and set of accompanying tools - that give designers everything they need to design a consistent conversational experience across any number of products and platforms."))
-  await addContent(frame1, "Principles", await createSlideFrame(TEMPLATE_BLOCKS, "Contributing", "Principles", "3. Add your own principles\n\nPrinciples keep foundational decisions consistent, and set precedent for how each component is used and built."))
-  await addContent(frame1, "Instructions", await createSlideFrame(TEMPLATE_INFO, "Contributing", "Instructions", "4. Add step-by-step instructions\n\nInclude instructions of where the assets can be found, how they are organized, how variants and overrides work, and any other details needed to use the library.", "1. Do this\n2. Do that\n3. Profit"))
+  await addContent(frame1, "Purpose", await createSlideFrame(TEMPLATE_INFO, "Using this library", "Purpose", "2. Describe the purpose\n\nThis library was created to fill a need. Describe that need and let designers what does (and doesn’t) fit within this library.\n\nYou can also add an image to the right that represents your library.", "E.g. The Conversation Kit is a Figma library - and set of accompanying tools - that give designers everything they need to design a consistent conversational experience across any number of products and platforms."))
+  await addContent(frame1, "Principles", await createSlideFrame(TEMPLATE_BLOCKS, "Using this library", "Principles", "3. Add your own principles\n\nPrinciples keep foundational decisions consistent, and set precedent for how each component is used and built."))
+  await addContent(frame1, "Instructions", await createSlideFrame(TEMPLATE_INFO, "Using this library", "Instructions", "4. Add step-by-step instructions\n\nInclude instructions of where the assets can be found, how they are organized, how variants and overrides work, and any other details needed to use the library.", "1) Do this\n\n2) Do that\n\n3) Profit"))
   figma.viewport.scrollAndZoomIntoView(figma.currentPage.children);
 
   return frame1
@@ -247,8 +259,8 @@ async function createContribute() {
 
   let frame1 = await createSlideFrame(TEMPLATE_CONTENTS, "Contributing", "", "1. Add sections here\n\nOptionally, add more sections to help describe how other designers can add to this library.")
   await addContent(frame1, "Conventions", await createSlideFrame(TEMPLATE_BLOCKS, "Contributing", "Conventions", "2. Add your own conventions\n\nWhat conventions does a designer need to be aware of to make components that work in a similar way to all the rest?"))
-  await addContent(frame1, "Instructions", await createSlideFrame(TEMPLATE_INFO, "Contributing", "Instructions", "3. Add step-by-step instructions\n\nInclude instructions on how to start a branch, organize pages, and request review.", "Create a Branch: Press the chevron next to the file name in the toolbar, and select Create branch.... Give it a name in the following format ➕<Component name>.\n\nDuplicate the ‘🚧 Component template’ page, and rename it according to the page naming converntions.\n\nPerform your explorations/work on this page.\n\nComplete the pre-publish checklist.\n\nRequest review by sharing the branch with <team slack channel or point-of-contact>"))
-  await addContent(frame1, "Checklist", await createSlideFrame(TEMPLATE_INFO, "Contributing", "Checklist", "4. Build a checklist\n\nWhat considerations do you go through before deciding if a component is ready to “go live”? These may refer back to your conventions.", "Did you do this?\nDid you do that?\nWhat about the other thing?"))
+  await addContent(frame1, "Instructions", await createSlideFrame(TEMPLATE_INFO, "Contributing", "Instructions", "3. Add step-by-step instructions\n\nInclude instructions on how to start a branch, organize pages, and request review.", "1) Create a Branch: Press the chevron next to the file name in the toolbar, and select Create branch.... Give it a name in the following format ➕<Component name>.\n\n2) Duplicate the ‘🚧 Component template’ page, and rename it according to the page naming converntions.\n\n3) Perform your explorations/work on this page.\n\n4) Complete the pre-publish checklist.\n\n5) Request review by sharing the branch with <team slack channel or point-of-contact>"))
+  await addContent(frame1, "Checklist", await createSlideFrame(TEMPLATE_INFO, "Contributing", "Checklist", "4. Build a checklist\n\nWhat considerations do you go through before deciding if a component is ready to “go live”? These may refer back to your conventions.", "☑️ Did you do this?\n☑️ Did you do that?\n☑️ What about the other thing?"))
   figma.viewport.scrollAndZoomIntoView(figma.currentPage.children);
 
   return frame1
@@ -259,7 +271,7 @@ async function createSlideFrame(id: string, supertitleText: string, titleText?: 
   let pageContents = figma.currentPage.children
   let lastAdded = pageContents[pageContents.length-1]
   let frame1 = figma.createFrame()
-  frame1.name = supertitleText
+  frame1.name = titleText ? supertitleText.concat("/", titleText) : supertitleText
   frame1.resize(1920, 1080)
   frame1.y = lastAdded ? lastAdded!.y + 1180 : 0
 
